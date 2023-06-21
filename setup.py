00ddd6d8c5817950f2292add364fca476de90c72
Iago <iagoo.feipe@gmail.com>
2023-06-20 22:47:12 -0300

    renamed:    __File.py -> my_tools/__File.py 	renamed:    __Registros.py -> my_tools/__Registros.py 	renamed:    __adm.py -> my_tools/__adm.py 	renamed:    __cpf.py -> my_tools/__cpf.py 	renamed:    __encode.py -> my_tools/__encode.py 	renamed:    __init__.py -> my_tools/__init__.py 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Conjunto de ferramentas Python'
+LONG_DESCRIPTION = 'Conjunto personalizado de ferramentas para manipulações de arquivos .txt, .json, .csv, manipulação de registros Windows e mais.'
+
+# Setting up
+setup(
+       # 'name' deve corresponder ao nome da pasta 'verysimplemodule'
+        name="my_tools", 
+        version=VERSION,
+        author="Iago Carvalho",
+        author_email="<iagoo.felipe123@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['unicodedata'], # adicione outros pacotes que 
+        # precisem ser instalados com o seu pacote. Ex: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
